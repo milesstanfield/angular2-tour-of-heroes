31295a31d8d4cc1e226415dd2c1e3ad2214a86b5
Miles <milesstanfield@gmail.com>
2016-05-26 22:56:15 -0400

    finished routing chapter

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,21 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero'
+import { RouteParams } from '@angular/router-deprecated';
+import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: 'app/hero-detail.component.html',
+  styleUrls: ['app/hero-detail.component.css']
 })
 
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private routeParams: RouteParams) {
+  }
   hero: Hero;
+
+  ngOnInit() {
+    let id = +this.routeParams.get('id');
+    this.heroService.getHero(id).then(hero => this.hero = hero);
+  }
+
+  goBack() {
+    window.history.back();
+  }
+
 }
